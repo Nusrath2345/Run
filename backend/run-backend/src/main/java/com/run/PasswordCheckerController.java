84276f5c166450af457b782d55feac21d190e346
Doolenny <callum.stewart999@gmail.com>
2026-04-26 15:24:37 +0100

    Working on Controller

--- backend/run-backend/src/main/java/com/run/PasswordCheckerController.java
+++ backend/run-backend/src/main/java/com/run/PasswordCheckerController.java
@@ -1,5 +1,8 @@
 package com.run;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,5 +11,17 @@ public class PasswordCheckerController
 
 {
 
-    
+   @PostMapping("/check")
+    public Map<String, Object> checkPassword(@RequestBody Map<String, String> request) 
+    {
+
+        String password = request.get("password");
+
+        String result = PasswordCheckerUtil.checkStrength(password);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("strength", result);
+
+        return response; 
+    }
 }
